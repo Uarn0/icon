931af97dd54920a07de9f5a995f3e2dbd68162a6
Uarn0 <136082024+Uarn0@users.noreply.github.com>
2023-06-11 22:20:51 +0300

    загрузив

---
+++ dada/fib/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int size;
+        size = int.Parse(Console.ReadLine());
+        finobocci(size);
+
+    }
+    static void finobocci(int size)
+    {
+        int a, b, c;
+        a = 0;
+        b = 1;
+        c = 0;
+        Console.Write("{0} {1} ", a, b);
+
+        for (int i = 2; i < size; i++)
+        {
+            c = a + b;
+            Console.Write("{0} ", c);
+            a = b;
+            b = c;
+        }
+        return;
+    }
+}
